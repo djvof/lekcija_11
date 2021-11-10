e81946054f5bbcab80cfbf8ab2304ec2a4bcc6bd
Vitaly <vit@bb-tech.eu>
2021-11-10 20:36:10 +0200

    classwork

---
+++ Classwork/NumberLargerThanTenException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Classwork
+{
+    public class NumberLargerThanTenException : Exception
+    {
+        public NumberLargerThanTenException()
+        {
+
+        }
+
+        public NumberLargerThanTenException(string message) : base(message)
+        {
+
+        }
+
+        public NumberLargerThanTenException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ Classwork/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int userNumber = GetNumber();
+                Console.WriteLine($"User entered: {userNumber}");
+            }
+            catch (NumberLargerThanTenException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.WriteLine("End of program");
+        }
+
+        private static int GetNumber()
+        {
+            Console.WriteLine("Ievadi skaitli");
+
+            string userNumberInput = Console.ReadLine();
+            int userNumber;
+
+            try
+            {
+                userNumber = int.Parse(userNumberInput);
+
+                if (userNumber > 10)
+                {
+                    throw new NumberLargerThanTenException("Aplikācija atbalsta skaitļus tikai līdz 10.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Hey tas nebija skaitlis: {userNumberInput}");
+                userNumber = GetNumber();
+            }
+
+            return userNumber;
+        }
+
+
+
+    }
+}
+
+
+
+
